700850615108de23cd49b63e034dee01d56bd9e3
auduongtansang <1712145@student.hcmus.edu.vn>
2019-11-30 09:33:05 +0700

    FourierTransform class template

---
+++ EndtermImageProcessing/FourierTransform.cpp
@@ -0,0 +1,29 @@
+#include "FourierTransform.h"
+
+FourierTransform::FourierTransform()
+{
+}
+
+FourierTransform::~FourierTransform()
+{
+}
+
+vector<complex<double>> FourierTransform::FFT(vector<complex<double>> signal)
+{
+	return vector<complex<double>>();
+}
+
+vector<complex<double>> FourierTransform::IFFT(vector<complex<double>> signal)
+{
+	return vector<complex<double>>();
+}
+
+vector<complex<double>> FourierTransform::FFT2(vector<complex<double>> signal)
+{
+	return vector<complex<double>>();
+}
+
+vector<complex<double>> FourierTransform::IFFT2(vector<complex<double>> signal)
+{
+	return vector<complex<double>>();
+}
---
+++ EndtermImageProcessing/FourierTransform.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include <complex>
+#include <vector>
+
+using namespace std;
+
+class FourierTransform
+{
+public:
+	FourierTransform();
+	~FourierTransform();
+
+	/*
+	Hàm biến đổi Fourier nhanh (thuận) cho tín hiệu rời rạc 1D
+	Số mẫu của mỗi chiều trong tín hiệu phải là lũy thừa của 2
+	*/
+	vector< complex<double> > FFT(vector< complex<double> > signal);
+
+	/*
+	Hàm biến đổi Fourier nhanh (nghịch) cho tín hiệu rời rạc 1D
+	Số mẫu của mỗi chiều trong tín hiệu phải là lũy thừa của 2
+	*/
+	vector< complex<double> > IFFT(vector< complex<double> > signal);
+
+	/*
+	Hàm biến đổi Fourier nhanh (thuận) cho tín hiệu rời rạc 2D
+	Số mẫu của mỗi chiều trong tín hiệu phải là lũy thừa của 2
+	*/
+	vector< complex<double> > FFT2(vector< complex<double> > signal);
+
+	/*
+	Hàm biến đổi Fourier nhanh (nghịch) cho tín hiệu rời rạc 2D
+	Số mẫu của mỗi chiều trong tín hiệu phải là lũy thừa của 2
+	*/
+	vector< complex<double> > IFFT2(vector< complex<double> > signal);
+};
