8bb099a1007d980394cf436f3325402868440869
auduongtansang <1712145@student.hcmus.edu.vn>
2019-11-30 11:15:28 +0700

    Direction enum class

--- EndtermImageProcessing/FourierTransform.cpp
+++ EndtermImageProcessing/FourierTransform.cpp
@@ -8,22 +8,24 @@ FourierTransform::~FourierTransform()
 {
 }
 
-vector<complex<double>> FourierTransform::FFT(vector<complex<double>> signal)
+vector<complex<double>> FourierTransform::FFT(vector<complex<double>> signal, Direction direction)
 {
 	/*
 	Trong biến đổi Fourier rời rạc, có:
 		F[k] = sum(X[i] * exp(-j * 2 * pi * k * i / n))
 		(với X là tín hiệu rời rạc có n mẫu, j là đơn vị ảo, 0 <= i, k <= n)
 
-	Chứng minh được:
-		F[k] = sum(X[u] * exp(-j * 2 * pi * k * u / m)) + exp(-j * 2 * pi * k / n) * sum(X[v] * exp(-j * 2 * pi * k * v / m))
-		(với m = n / 2, u là số chẵn, v là số lẻ, 0 <= u, v, k <= n)
+		Chứng minh được:
+			F[k] = sum(X[u] * exp(-j * 2 * pi * k * u / m)) + exp(-j * 2 * pi * k / n) * sum(X[v] * exp(-j * 2 * pi * k * v / m))
+			(với m = n / 2, u là số chẵn, v là số lẻ, 0 <= u, v, k <= n)
 
-	Do đó:
-		F[k] = F_của_các_mẫu_tại_vị_trí_chẵn[k] + hệ số * F_của_các_mẫu_tại_vị_trí_lẻ[k]
-		=> Gọi đệ quy để tính toán
+		Do đó:
+			F[k] = F_của_các_mẫu_tại_vị_trí_chẵn[k] + hệ số * F_của_các_mẫu_tại_vị_trí_lẻ[k]
+			=> Gọi đệ quy để tính toán.
 
-	Hơn nữa, với tín hiệu có n mẫu, miền tần số sẽ lặp lại sau mỗi n / 2 mẫu
+		Hơn nữa, với tín hiệu có n mẫu, miền tần số sẽ lặp lại sau mỗi n / 2 mẫu.
+
+	Tương tự cho phép biến đổi nghịch.
 	*/
 
 	int n = (int)(signal.size());
@@ -48,37 +50,32 @@ vector<complex<double>> FourierTransform::FFT(vector<complex<double>> signal)
 	}
 
 	//Gọi đệ quy biến đổi Fourier trên 2 tín hiệu này
-	vector<complex<double>> freqEven = FFT(signalEven);
-	vector<complex<double>> freqOdd = FFT(signalOdd);
+	vector<complex<double>> freqEven = FFT(signalEven, direction);
+	vector<complex<double>> freqOdd = FFT(signalOdd, direction);
 
 	//Tính toán kết quả theo công thức đã chứng minh
 	vector<complex<double>> freqFinal(n);
-	for (int i = 0; i < half; i++)
-	{
-		complex<double> oddCoefficent = polar(1.0, -2.0 * 3.14 * i / n);
 
-		/*
-		F[k] = F_của_các_mẫu_tại_vị_trí_chẵn[k] + hệ số * F_của_các_mẫu_tại_vị_trí_lẻ[k]
-		Miền tần số lặp lại nhưng nghịch dấu ở tín hiệu lẻ
-		*/
-		freqFinal[i] = freqEven[i] + oddCoefficent * freqOdd[i];
-		freqFinal[i + half] = freqEven[i] - oddCoefficent * freqOdd[i];
+	if (direction == Direction::Forward)
+	{
+		for (int i = 0; i < half; i++)
+		{
+			//Hệ số
+			complex<double> oddCoefficent = polar(1.0, -2.0 * 3.14 * i / n);
+
+			/*
+			F[k] = F_của_các_mẫu_tại_vị_trí_chẵn[k] + hệ số * F_của_các_mẫu_tại_vị_trí_lẻ[k]
+			Miền tần số lặp lại nhưng nghịch dấu ở các mẫu tại vị trí lẻ
+			*/
+			freqFinal[i] = freqEven[i] + oddCoefficent * freqOdd[i];
+			freqFinal[i + half] = freqEven[i] - oddCoefficent * freqOdd[i];
+		}
 	}
 
 	return freqFinal;
 }
 
-vector<complex<double>> FourierTransform::IFFT(vector<complex<double>> signal)
-{
-	return vector<complex<double>>();
-}
-
-vector<complex<double>> FourierTransform::FFT2(vector<complex<double>> signal)
-{
-	return vector<complex<double>>();
-}
-
-vector<complex<double>> FourierTransform::IFFT2(vector<complex<double>> signal)
+vector<complex<double>> FourierTransform::FFT2(vector<complex<double>> signal, Direction direction)
 {
 	return vector<complex<double>>();
 }
--- EndtermImageProcessing/FourierTransform.h
+++ EndtermImageProcessing/FourierTransform.h
@@ -1,8 +1,21 @@
 ﻿#pragma once
+#define _USE_MATH_DEFINES
+
 #include <complex>
+#include <math.h>
 #include <vector>
 
 using namespace std;
+
+//Enum xác định chiều của biến đổi Fourier (thuận hay nghịch)
+enum class Direction
+{
+	//Thuận
+	Forward,
+
+	//Nghịch
+	Backward
+};
 
 class FourierTransform
 {
@@ -11,26 +24,14 @@ public:
 	~FourierTransform();
 
 	/*
-	Hàm biến đổi Fourier nhanh (thuận) cho tín hiệu rời rạc 1D
+	Hàm biến đổi Fourier nhanh cho tín hiệu rời rạc 1D
 	Số mẫu của mỗi chiều trong tín hiệu phải là lũy thừa của 2
 	*/
-	vector<complex<double>> FFT(vector<complex<double>> signal);
+	vector<complex<double>> FFT(vector<complex<double>> signal, Direction direction);
 
 	/*
-	Hàm biến đổi Fourier nhanh (nghịch) cho tín hiệu rời rạc 1D
+	Hàm biến đổi Fourier nhanh cho tín hiệu rời rạc 2D
 	Số mẫu của mỗi chiều trong tín hiệu phải là lũy thừa của 2
 	*/
-	vector<complex<double>> IFFT(vector<complex<double>> signal);
-
-	/*
-	Hàm biến đổi Fourier nhanh (thuận) cho tín hiệu rời rạc 2D
-	Số mẫu của mỗi chiều trong tín hiệu phải là lũy thừa của 2
-	*/
-	vector<complex<double>> FFT2(vector<complex<double>> signal);
-
-	/*
-	Hàm biến đổi Fourier nhanh (nghịch) cho tín hiệu rời rạc 2D
-	Số mẫu của mỗi chiều trong tín hiệu phải là lũy thừa của 2
-	*/
-	vector<complex<double>> IFFT2(vector<complex<double>> signal);
+	vector<complex<double>> FFT2(vector<complex<double>> signal, Direction direction);
 };
