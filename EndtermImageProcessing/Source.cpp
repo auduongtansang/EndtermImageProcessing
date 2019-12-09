e55476950b12ea497385a182dcda97f8548fc043
auduongtansang <1712145@student.hcmus.edu.vn>
2019-12-09 10:45:21 +0700

    Command line processing

--- EndtermImageProcessing/Source.cpp
+++ EndtermImageProcessing/Source.cpp
@@ -5,28 +5,106 @@
 using namespace std;
 using namespace cv;
 
-int main()
+int main(int argc, char **argv)
 {
-	//Load ảnh test
-	Mat img = imread("periodicLow.png", IMREAD_GRAYSCALE);
-	imshow("Original", img);
+	if (argc < 4)
+		return -1;
 
-	//Chuyển sang dạng ảnh phức
-	ComplexImage cImg;
-	cImg.FromOpenCVMat(img);
+	Mat img, dst;
 
-	//Biến đổi Fourier thuận
-	cImg.ForwardFourierTransform();
-	imshow("Frequency", cImg.ToOpenCVMat());
+	if (strcmp(argv[1], "--fft") == 0)
+	{
+		//Load ảnh
+		img = imread(argv[2], IMREAD_GRAYSCALE);
+		imshow("Original", img);
 
-	//Lọc tần số
-	FrequencyFilter filter(200, 312, 390, 634);
-	filter.Apply(cImg);
-	imshow("Filtered", cImg.ToOpenCVMat());
+		//Chuyển sang dạng ảnh phức
+		ComplexImage cImg;
+		cImg.FromOpenCVMat(img);
 
-	//Biến đổi Fourier nghịch
-	cImg.BackwardFourierTransform();
-	imshow("Inverse", cImg.ToOpenCVMat());
+		//Biến đổi Fourier thuận
+		cImg.ForwardFourierTransform();
+		dst = cImg.ToOpenCVMat();
+
+		//Ghi và hiển thị ảnh
+		imwrite(argv[3], dst);
+		imshow("Frequency", dst);
+	}
+	else if (strcmp(argv[1], "--ifft") == 0)
+	{
+		//Load ảnh
+		img = imread(argv[2], IMREAD_GRAYSCALE);
+		imshow("Original", img);
+
+		//Chuyển sang dạng ảnh phức
+		ComplexImage cImg;
+		cImg.FromOpenCVMat(img);
+
+		//Biến đổi Fourier thuận
+		cImg.ForwardFourierTransform();
+		imshow("Frequency", cImg.ToOpenCVMat());
+
+		//Biến đổi Fourier nghịch
+		cImg.BackwardFourierTransform();
+		dst = cImg.ToOpenCVMat();
+
+		//Ghi và hiển thị ảnh
+		imwrite(argv[3], dst);
+		imshow("Inversion", dst);
+	}
+	else if (strcmp(argv[1], "--freq") == 0)
+	{
+		if (argc < 8)
+			return -1;
+
+		//Load ảnh
+		img = imread(argv[2], IMREAD_GRAYSCALE);
+		imshow("Original", img);
+
+		//Chuyển sang dạng ảnh phức
+		ComplexImage cImg;
+		cImg.FromOpenCVMat(img);
+
+		//Biến đổi Fourier thuận
+		cImg.ForwardFourierTransform();
+		imshow("Frequency", cImg.ToOpenCVMat());
+
+		int minX = stoi(argv[4]), maxX = stoi(argv[5]), minY = stoi(argv[6]), maxY = stoi(argv[7]);
+		FrequencyFilter filter(minX, maxX, minY, maxY);
+		filter.Apply(cImg);
+
+		//Hiển thị ảnh tần số đã lọc
+		imshow("Filtered (frequency)", cImg.ToOpenCVMat());
+
+		//Biến đổi Fourier nghịch
+		cImg.BackwardFourierTransform();
+		dst = cImg.ToOpenCVMat();
+
+		//Ghi và hiển thị ảnh
+		imwrite(argv[3], dst);
+		imshow("Filtered (inversion)", dst);
+	}
+
+	////Load ảnh test
+	//Mat img = imread("periodicLow.png", IMREAD_GRAYSCALE);
+	//imshow("Original", img);
+
+	////Chuyển sang dạng ảnh phức
+	//ComplexImage cImg;
+	//cImg.FromOpenCVMat(img);
+
+	////Biến đổi Fourier thuận
+	//cImg.ForwardFourierTransform();
+	//imshow("Frequency", cImg.ToOpenCVMat());
+
+	////Lọc tần số
+	//FrequencyFilter filter(200, 312, 390, 634);
+	//filter.Apply(cImg);
+	//imshow("Filtered", cImg.ToOpenCVMat());
+
+	////Biến đổi Fourier nghịch
+	//cImg.BackwardFourierTransform();
+	//imshow("Inverse", cImg.ToOpenCVMat());
 
 	waitKey(0);
 	return 0;
